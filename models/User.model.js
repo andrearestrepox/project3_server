ad5dfa3961928e5fb62e92d4e6c6f7f50b36e598
Andrea Restrepo <andrearestrepox@gmail.com>
2022-07-28 19:15:46 -0400

    adding existing files

---
+++ models/User.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, 'Name is required']
+      // unique: true -> Ideally, should be unique, but its up to you
+    },
+  
+
+    email: {
+      type: String, 
+      required: [true, 'Email is required.'],
+      unique: true, 
+      trim: true
+    },
+
+    passwordHash: {
+      type: String,
+      require: [true, 'Password is required.']
+    }
+  },
+
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("project3_backend JS imported successfully!");
+  },
+  false
+);
