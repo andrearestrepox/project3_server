fa4a58cd105166b65d2cf0e8b48a1ed97171761e
Andrea Restrepo <andrearestrepox@gmail.com>
2022-08-04 18:17:18 -0400

    working edit

--- routes/user-profile.routes.js
+++ routes/user-profile.routes.js
@@ -110,12 +110,111 @@ router.get("/users", isAuthenticated, (req, res, next) => {
 
 });
 
-router.get("/myprofile", isAuthenticated, (res, req, next) => {
-    User.findById()
-        .populate('profileId')
-        .then((individualUserInfo) => res.json(individualUserInfo))
-        .catch((err) => res.json(err));
+// router.get("/myprofile", isAuthenticated, (req, res, next) => {
+//     User.findById()
+//         .populate('profileId')
+//         .then((individualUserInfo) => res.json(individualUserInfo))
+//         .catch((err) => res.json(err));
+// })
+router.get("/individual-profile/my-data", isAuthenticated, (req, res, next) => {
+    const profileId = req.payload.profileId;
+    console.log(profileId)
+    Profile.findById(profileId)
+    .populate({path:'answerOne answerTwo answerThree answerFour answerFive',populate: {path:'questionId', model:'Question'}})
+    .then((responseOne) => {
+   console.log(responseOne)
+    res.json(responseOne)
+    })
+    .catch((err) => res.json(err));
 })
+
+router.get("/individual-profile/:profileId", (req, res, next) => {
+    const {profileId} = req.params
+    console.log(profileId)
+    Profile.findById(profileId)
+    .populate({path:'answerOne answerTwo answerThree answerFour answerFive',populate: {path:'questionId', model:'Question'}})
+    .then((responseOne) => {
+   console.log(responseOne)
+    res.json(responseOne)
+})
+        
+});
+router.delete("/individual-profile", isAuthenticated, (req, res, next) => {
+const userId = req.payload._id
+console.log(userId)
+User.findByIdAndRemove(userId)
+.then((response) => {
+   
+    console.log(response, 'it works')
+    res.status(200).json({message: 'Deleted user successfully.'})
+})
+.catch((err) => console.log(err))
+});
+
+
+router.post("/edit-profile", isAuthenticated, (req, res, next) => {
+    const {
+        bio,
+        answerOne,
+        answerTwo,
+        answerThree,
+        answerFour,
+        answerFive,
+        questionOne,
+        questionTwo,
+        questionThree,
+        questionFour,
+        questionFive
+    } = req.body;
+    console.log(req.body, req.payload)
+    //create 5 answers
+    Profile.findById(req.payload.profileId)
+        .populate('answerOne answerTwo answerThree answerFour answerFive')
+        .then(foundProfile => {
+            foundProfile.answerOne.userAnswer = answerOne;
+            foundProfile.answerTwo.userAnswer = answerTwo;
+            foundProfile.answerThree.userAnswer = answerThree;
+            foundProfile.answerFour.userAnswer = answerFour;
+            foundProfile.answerFive.userAnswer = answerFive;
+            foundProfile.answerOne.questionId = questionOne;
+            foundProfile.answerTwo.questionId = questionTwo;
+            foundProfile.answerThree.questionId = questionThree;
+            foundProfile.answerFour.questionId = questionFour;
+            foundProfile.answerFive.questionId = questionFive;
+            return Promise.all([
+                foundProfile.answerOne.save(),
+                 foundProfile.answerTwo.save(),
+                 foundProfile.answerThree.save(),
+                  foundProfile.answerFour.save(),
+                  foundProfile.answerFive.save(),
+            ])
+        })
+        .then(updatedAnswerArray => {
+           
+            res.status(200).json({message: 'Answers updated'})
+        })
+         .catch((err) => res.status(500).json(err));
+        
+
+    //then create profile with 5 answer ids
+
+
+})
+
+// router.get("/individual-profile/:questionId", (req, res, next) => {
+//     const {questionId} = req.params
+//     console.log(questionId)
+//     Question.findById(questionId)
+//     .populate('questionOne questionTwo questionThree questionFour questionFive')
+//     .then((response) => {
+//         console.log(response)
+//         res.json(response)
+//     })
+
+// });
+
+
+
 
 module.exports = router;
 
